80538d8b69e7c081444f23d0561e8aaef73e378f
Chqves <erick.chaves.delgado@gmail.com>
2026-04-16 18:47:27 -0600

    Commit 1

---
+++ src/test/java/com/eventosapp/plataforma/PlataformaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.eventosapp.plataforma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
